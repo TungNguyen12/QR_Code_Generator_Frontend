5ffe86139de875b6caacd9d58bcd4565fba34e53
TungNguyen12 <nvtung382@gmail.com>
2025-01-10 17:42:36 +0200

    fix type

--- src/pages/auth/CheckLogin.tsx
+++ src/pages/auth/CheckLogin.tsx
@@ -1,12 +1,7 @@
 import { Navigate } from 'react-router-dom'
 import { useAppSelector } from '../../hooks/useAppSelector'
-import React from 'react'
 
-interface CheckLoginProps {
-  children: React.ReactNode
-}
-
-const CheckLogin: React.FC<CheckLoginProps> = ({ children }) => {
+const CheckLogin = ({ children }: any) => {
   const validUser = useAppSelector((state) => state.auth.token)
 
   return validUser ? children : <Navigate to="/login" />
